966960f6dc21da4fb6c4af3b84196b5246ce24fa
qu3vipon <sdh5813@gmail.com>
2024-06-08 22:54:38 +0900

    chore: add transaction decorator

--- src/product/service/order.py
+++ src/product/service/order.py
@@ -11,97 +11,95 @@ from user.models import ServiceUser, UserPointsHistory, UserPoints
 
 class OrderService:
     @staticmethod
+    @transaction.atomic
     def create_order(
         user: ServiceUser,
         products: List[Product],
         product_id_to_quantity: Dict[int, int],
     ) -> Order:
-        with transaction.atomic():
-            total_price: int = 0
-            order = Order.objects.create(user=user)
+        total_price: int = 0
+        order = Order.objects.create(user=user)
 
-            order_lines_to_create: List[OrderLine] = []
-            for product in products:
-                price: int = product.price
-                discount_ratio: float = 0.9
-                quantity: int = product_id_to_quantity[product.id]
+        order_lines_to_create: List[OrderLine] = []
+        for product in products:
+            price: int = product.price
+            discount_ratio: float = 0.9
+            quantity: int = product_id_to_quantity[product.id]
 
-                order_lines_to_create.append(
-                    OrderLine(
-                        order=order,
-                        product=product,
-                        quantity=quantity,
-                        price=price,
-                        discount_ratio=discount_ratio,
-                    )
+            order_lines_to_create.append(
+                OrderLine(
+                    order=order,
+                    product=product,
+                    quantity=quantity,
+                    price=price,
+                    discount_ratio=discount_ratio,
                 )
+            )
 
-                total_price += price * quantity * discount_ratio
+            total_price += price * quantity * discount_ratio
 
-            order.total_price = int(total_price)
-            order.save()
-            OrderLine.objects.bulk_create(objs=order_lines_to_create)
+        order.total_price = int(total_price)
+        order.save()
+        OrderLine.objects.bulk_create(objs=order_lines_to_create)
         return order
 
     @staticmethod
+    @transaction.atomic
     def confirm_order(user_id: int, order: Order) -> None:
-        with transaction.atomic():
-            success: int = Order.objects.filter(
-                id=order.id, status=OrderStatus.PENDING
-            ).update(status=OrderStatus.PAID)
-            if not success:
-                raise OrderAlreadyPaidException
+        success: int = Order.objects.filter(
+            id=order.id, status=OrderStatus.PENDING
+        ).update(status=OrderStatus.PAID)
+        if not success:
+            raise OrderAlreadyPaidException
 
-            user = ServiceUser.objects.get(id=user_id)
-            if user.points < order.total_price:
-                raise UserPointsNotEnoughException
+        user = ServiceUser.objects.get(id=user_id)
+        if user.points < order.total_price:
+            raise UserPointsNotEnoughException
 
-            success: int = ServiceUser.objects.filter(
-                id=user_id, version=user.version
-            ).update(
-                points=F("points") - order.total_price,
-                order_count=F("order_count") + 1,
-                version=user.version + 1,
-            )
+        success: int = ServiceUser.objects.filter(
+            id=user_id, version=user.version
+        ).update(
+            points=F("points") - order.total_price,
+            order_count=F("order_count") + 1,
+            version=user.version + 1,
+        )
 
-            if not success:
-                raise UserVersionConflictException
+        if not success:
+            raise UserVersionConflictException
 
-            UserPointsHistory.objects.create(
-                user=user,
-                points_change=-order.total_price,
-                reason=f"orders:{order.id}:confirm",
-            )
+        UserPointsHistory.objects.create(
+            user=user,
+            points_change=-order.total_price,
+            reason=f"orders:{order.id}:confirm",
+        )
 
     @staticmethod
+    @transaction.atomic
     def confirm_order_v2(user_id: int, order: Order) -> None:
-        with transaction.atomic():
-            success: int = Order.objects.filter(
-                id=order.id, status=OrderStatus.PENDING
-            ).update(status=OrderStatus.PAID)
-            if not success:
-                raise OrderAlreadyPaidException
+        success: int = Order.objects.filter(
+            id=order.id, status=OrderStatus.PENDING
+        ).update(status=OrderStatus.PAID)
+        if not success:
+            raise OrderAlreadyPaidException
 
-            last_points = (
-                UserPoints.objects.filter(user_id=user_id).order_by("-version").first()
-            )
-            if last_points.points_sum < order.total_price:
-                raise UserPointsNotEnoughException
+        last_points = (
+            UserPoints.objects.filter(user_id=user_id).order_by("-version").first()
+        )
+        if last_points.points_sum < order.total_price:
+            raise UserPointsNotEnoughException
 
-            UserPoints.objects.create(
-                user_id=user_id,
-                version=last_points.version + 1,
-                points_change=-order.total_price,
-                points_sum=last_points.points_sum - order.total_price,
-                reason=f"orders:{order.id}:confirm",
-            )
+        UserPoints.objects.create(
+            user_id=user_id,
+            version=last_points.version + 1,
+            points_change=-order.total_price,
+            points_sum=last_points.points_sum - order.total_price,
+            reason=f"orders:{order.id}:confirm",
+        )
 
-            if not success:
-                raise UserVersionConflictException
+        if not success:
+            raise UserVersionConflictException
 
-            ServiceUser.objects.filter(id=user_id).update(
-                order_count=F("order_count") + 1
-            )
+        ServiceUser.objects.filter(id=user_id).update(order_count=F("order_count") + 1)
 
 
 order_service = OrderService()
